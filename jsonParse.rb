85cc64e622142a845f108d4d41a0302df57bc000
dawiest <daryl.wiest@gmail.com>
2015-05-07 23:49:32 -0400

    Solution after having cdownard from codenewbie.org's slack chat help me!

--- jsonParse.rb
+++ jsonParse.rb
@@ -8,32 +8,20 @@ json = file.read
 
 parsed = JSON.parse(json)
 
-parsed["Instances"].each do |inst|
-  ip=nil
-  tags=Hash.new
-  is_running=false
-  state=nil
-     
-  inst.each do |key, value|
-
-    case key
-      when "PrivateIpAddress"
-        ip = value
-      when "State"
-        is_running = value["Name"]=="running"
-        state = value["Name"]
-      when "Tags"
-        value.each do |tag|
-          tags[tag["Key"]]=tag["Value"]
-       end
+jsonObject = parsed
+#parsed.each do |jsonObject|
+  hashes = jsonObject["Instances"].map do |instance|
+    h = {}.tap do |hash|
+      hash[:private_ip_address] = instance["PrivateIpAddress"]
+      hash[:instance_id] = instance["InstanceId"]
+      hash[:state] = instance["State"]["Name"]
+      hash[:name] = instance["Tags"].find { |tag| tag["Value"] if tag["Key"] == "Name" }
     end
+#    p h
+    h
   end
-
-  ret1 = [tags["Name"], ip, state, tags["Env"]] 
-  p ret1
-  ret2 = {:name => tags["Name"], :ip => ip, :state => state, :env=>tags["Env"]}
-  p ret2
-end
-
+  p hashes
+  hashes
+#end
 
 
